855e3fcac7a0a095611c9a52c870c23837206fab
Mercedes Ubeira Romero <mercedesubeira@gmail.com>
2023-09-26 09:27:34 +0200

    bjhbbg

--- counter.js
+++ counter.js
@@ -4,14 +4,17 @@
 const plusButtons = document.querySelectorAll(".plusbutton");
 const minusButtons = document.querySelectorAll(".minusbutton");
 
-//Now we add an eventlistener, which basically means that we tell the browser that when something happens (in this case when the user clicks the btn), some function will be activated.
+//Now we add an eventlistener, which basically means that we tell the browser that when something 
+// happens (in this case when the user clicks the btn), some function will be activated.
 // the "forEach" means that this is gonna happen in each of the buttons, and not only the first one.
 
 plusButtons.forEach((plusButton) => {
   plusButton.addEventListener("click", () => {
     const counternumber =
-      plusButton.parentElement.querySelector(".counternumber"); //then we go and select the counternumber, which is 0 in our HTML file.
-    counternumber.textContent = parseInt(counternumber.textContent) + 1; //so every time the user clicks the btn we will get that value, whatever it is and add 1 and return the sum.
+      plusButton.parentElement.querySelector(".counternumber"); 
+      //then we go and select the counternumber, which is 0 in our HTML file.
+    counternumber.textContent = parseInt(counternumber.textContent) + 1;
+    //so every time the user clicks the btn we will get that value, whatever it is and add 1 and return the sum.
   });
 });
 
